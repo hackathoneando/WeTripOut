a4c48ea4f535e04866bba27ff28caa465e64ce45
Andryut <aahuertasca@unal.edu.co>
2019-01-26 12:46:59 -0500

    Depedencies deleted from the main file.

--- UNicode/we_trip_out/lib/main.dart
+++ UNicode/we_trip_out/lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_functions/cloud_functions.dart';
 
 void main() => runApp(MyApp());
 
