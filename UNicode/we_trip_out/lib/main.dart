b50398e2a8a9341b9b9cd4051aa7153494d48b9e
Andryut <aahuertasca@unal.edu.co>
2019-01-26 01:28:46 -0500

    Dependencies solved for android

--- UNicode/we_trip_out/lib/main.dart
+++ UNicode/we_trip_out/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-//import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_functions/cloud_functions.dart';
 
 void main() => runApp(MyApp());
 
